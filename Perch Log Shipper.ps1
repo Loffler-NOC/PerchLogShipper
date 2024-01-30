c4a2f67a164a0914770898221f94ff5d54d2a35a
Loffler-NOC <150292433+Loffler-NOC@users.noreply.github.com>
2024-01-30 14:23:26 -0600

    Add files via upload

---
+++ Perch Log Shipper.ps1
@@ -0,0 +1,19 @@
+# Define the URL to download the .EXE from
+$downloadUrl = "https://cdn.perchsecurity.com/downloads/perch-log-shipper-latest.exe"
+
+# Specify the token value
+$Token = "$ENV:PerchToken"
+
+# Define the local path to save the downloaded .EXE file
+$downloadPath = "C:\temp\perch-log-shipper-latest.exe"
+
+# Download the .EXE file
+Invoke-WebRequest -Uri $downloadUrl -OutFile $downloadPath
+
+# Check if the download was successful
+if (Test-Path $downloadPath) {
+    # Run the .EXE with the specified arguments
+    Start-Process -FilePath $downloadPath -ArgumentList "/qn", "OUTPUT=TOKEN", "VALUE=$Token" -Wait
+} else {
+    Write-Host "Failed to download the file from $downloadUrl"
+}
